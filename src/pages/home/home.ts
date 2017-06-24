4a87b4fe6d40217c58d62f454cf7636b598ef0d8
peterennis <peterennis@yahoo.com>
2017-06-24 16:31:08 -0700

    ionic basic integration with angular tree component

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { TreeModule } from 'angular-tree-component';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  nodes = [
+    {
+      id: 1,
+      name: 'root1',
+      children: [
+        { id: 2, name: 'child1' },
+        { id: 3, name: 'child2' }
+      ]
+    },
+    {
+      id: 4,
+      name: 'root2',
+      children: [
+        { id: 5, name: 'child2.1' },
+        {
+          id: 6,
+          name: 'child2.2',
+          children: [
+            { id: 7, name: 'subsub' }
+          ]
+        }
+      ]
+    }
+  ];
 
   constructor(public navCtrl: NavController) {
 
